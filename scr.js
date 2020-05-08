90dd18c61a9dfa7d12b25797cc078a6cd430cb92
Strelok-SV <streloksv036@gmail.com>
2020-05-08 17:00:01 +0300

    I DONT KNOW

--- scr.js
+++ scr.js
@@ -1,13 +1,5 @@
 function swapImage() {
-    let img1 = document.getElementById("image1");
-    let img2 = document.getElementById("image2");
+    let img = document.getElementById("image");
 
-    if (img1.style.display == "block") {
-        img1.style.display = "none";
-        img2.style.display = "block";
-    }
-    else {
-        img1.style.display = "block";
-        img2.style.display = "none";
-    }
+    img.classList.toggle("svoboda-image");
 }
